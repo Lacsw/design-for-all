31acdf5502c33ff1270818df6d582fbd4e8e1c98
Frolov Roman <frolloff1@yandex.ru>
2024-03-15 20:48:10 +0300

    fix(build): assets without hash-names prefix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig(() => {
     base: '/',
     build: {
       outDir: 'build',
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
+      },
     },
     plugins: [
       react(),
