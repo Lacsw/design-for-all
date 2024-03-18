8912b8d45bb6134b209c386787d5aa47106b9f0c
Kikodze Viktor <vitland-x@yandex.ru>
2024-03-18 23:58:36 +0300

    feat: change exports

--- src/components/index.js
+++ src/components/index.js
@@ -49,3 +49,4 @@ export { default as SearchInput } from './SearchInput/SearchInput';
 export { default as Reviewers } from './Reviewers/Reviewers';
 export { default as SideBar } from './SideBar/SideBar';
 export { default as Map } from './Map/Map';
+export { default as MainNavigationBar } from './MainNavigationBar/MainNavigationBar';
