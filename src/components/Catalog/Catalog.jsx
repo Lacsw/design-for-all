81ea70b8f8b29361b3443283b94c947c37a7fe18
Cheremis <alseta88@yandex.ru>
2024-08-04 12:55:52 +0300

    scroll-up new page

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,10 +1,13 @@
+import { useEffect, useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 import { SideBar } from 'components';
 import './Catalog.css';
 
 export default function Catalog() {
+  const catalogRef = useRef();
+  useEffect(() => catalogRef.current.scrollIntoView(false), []);
   return (
-    <div className="catalog__container">
+    <div className="catalog__container" ref={catalogRef}>
       <SideBar />
       <Outlet />
     </div>
