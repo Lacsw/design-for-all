5a67ccde65b19a83d32c48dfd6f6af8e47ba5c20
Cheremis <alseta88@yandex.ru>
2024-08-19 16:25:58 +0300

    Updates components

---
+++ src/components/Updates/Updates.jsx
@@ -0,0 +1,12 @@
+import UpdateCard from './UpdateCard';
+import './Updates.css';
+
+export default function Updates() {
+  return (
+    <section className='updates-slide'>
+      <h2 className='updates-slide__title'>Обновления</h2>
+      <ul className='updates-slide__list'>
+      </ul>
+    </section>
+  )
+}
