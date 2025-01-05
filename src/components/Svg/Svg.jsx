0e3019cf8cf4cf1bb0a79f009837cc60e8e1bdad
Cheremis <alseta88@yandex.ru>
2025-01-05 14:21:42 +0300

    try 2

--- src/components/Svg/Svg.jsx
+++ src/components/Svg/Svg.jsx
@@ -2,7 +2,7 @@ import './Svg.css';
 
 const Svg = ({ icon }) => {
   const mask = `url(${icon}) no-repeat center / 100%`;
-  return <div className="svg-box" style={{ maskImage: `url(${icon})` }} />;
+  return <div className="svg-box" style={{ WebkitMaskImage: `url(${icon})`, color: 'red' }} />;
 };
 
 export default Svg;
