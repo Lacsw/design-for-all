88896def63e27a7884dfc22d13b99bd92c9a5fa6
Cheremis <alseta88@yandex.ru>
2025-01-05 14:06:20 +0300

    try to fix mask

--- src/components/Svg/Svg.jsx
+++ src/components/Svg/Svg.jsx
@@ -2,7 +2,7 @@ import './Svg.css';
 
 const Svg = ({ icon }) => {
   const mask = `url(${icon}) no-repeat center / 100%`;
-  return <div className="svg-box" style={{ mask }} />;
+  return <div className="svg-box" style={{ maskImage: `url(${icon})` }} />;
 };
 
 export default Svg;
