6543c78727f0f4e2c41cc1f041727e18408d7f0a
Cheremis <alseta88@yandex.ru>
2025-01-19 14:25:45 +0300

    feat: expand catalog items for article

--- src/components/ArticlesTree/TreeItem/TreeItem.jsx
+++ src/components/ArticlesTree/TreeItem/TreeItem.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { TreeList } from 'components';
 import './TreeItem.css';
 import { useSelector } from 'react-redux';
 import { getCurrentTheme } from 'store/selectors';
+import findId from './findId';
 
-export default function TreeItem({ title, data, language }) {
+export default function TreeItem({ title, data, language, status }) {
   const navigate = useNavigate();
+  const { articleId } = useParams();
   const theme = useSelector(getCurrentTheme);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(() => {
+    if (status === false) return false;
+    return findId(data, articleId);
+  });
   const hasChildren = typeof data === 'object';
   const id = hasChildren ? data.id : data;
   const newData = { ...data };
@@ -43,7 +48,7 @@ export default function TreeItem({ title, data, language }) {
         </span>
       </div>
       {!isOpen || !hasChildren ? null : (
-        <TreeList list={newData} language={language} />
+        <TreeList list={newData} language={language} status={isOpen} />
       )}
     </li>
   );
