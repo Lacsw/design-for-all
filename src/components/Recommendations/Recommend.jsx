16314b2b0ce27854039f252ee2d8ae8a5c1d4636
Cheremis <alseta88@yandex.ru>
2025-01-30 02:47:07 +0300

    add default image for recommends and article

--- src/components/Recommendations/Recommend.jsx
+++ src/components/Recommendations/Recommend.jsx
@@ -1,18 +1,29 @@
-import defaultImage from '../../images/article/recommend.png';
+import { useSelector } from 'react-redux';
+import imgBlack from '../../images/error-image_black.jpg';
+import imgWhite from '../../images/error-image_white.jpg';
 import './Recommend.css';
+import { getCurrentTheme } from 'store/selectors';
+
+function cropTitle(title) {
+  if (!title) return 'Заголовок отсутствует';
+  if (title.length > 40) return title?.slice(0, 35) + '...';
+  return title;
+}
 
 const Recommend = ({ imageUrl, title }) => {
+  const theme = useSelector(getCurrentTheme);
+  const defaultImage = theme === 'dark' ? imgBlack : imgWhite;
+
   return (
     <div className="recommend">
       <img
+        key={theme}
         src={!imageUrl || imageUrl.includes('test_') ? defaultImage : imageUrl}
         alt="Превью статьи"
         className="recommend__image"
-        onError={evt => evt.target.src = defaultImage}
+        onError={(evt) => (evt.target.src = defaultImage)}
       />
-      <h4 className="recommend__title">
-        {title?.slice(0, 40) || 'Заголовок отсутствует'}
-      </h4>
+      <h4 className="recommend__title">{cropTitle(title)}</h4>
     </div>
   );
 };
