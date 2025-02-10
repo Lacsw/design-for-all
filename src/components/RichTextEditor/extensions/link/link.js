e0bb5ee3700fe01587ab2392206866781b015401
Kargobober <kargobober@yandex.ru>
2025-02-10 23:32:08 +0300

    temp

--- src/components/RichTextEditor/extensions/link/link.js
+++ src/components/RichTextEditor/extensions/link/link.js
@@ -1,5 +1,6 @@
 // @ts-check
 import Link, { isAllowedUri } from '@tiptap/extension-link';
+import { Plugin, PluginKey, TextSelection } from '@tiptap/pm/state';
 
 export const CustomLinkExtension = Link.extend({
   renderHTML({ HTMLAttributes }) {
@@ -31,4 +32,59 @@ export const CustomLinkExtension = Link.extend({
       0,
     ];
   },
+
+  // addProseMirrorPlugins() {
+  //   return [
+  //     new Plugin({
+  //       key: new PluginKey('eventHandler'),
+  //       props: {
+  //         handleKeyDown(view, pos, event) {
+  //           const { state } = view;
+  //           const { selection } = state;
+  //           const { from, to } = selection;
+
+  //           let spaceCount = 0;
+
+  //           // Проверяем, что нажата клавиша "пробел"
+  //           if (event.key === ' ') {
+  //             // Проверяем, находится ли каретка внутри марки "link"
+  //             const marksAtCursor = state.doc.rangeHasMark(
+  //               from,
+  //               to,
+  //               state.schema.marks.link
+  //             );
+
+  //             if (marksAtCursor) {
+  //               spaceCount++; // Увеличиваем счётчик
+
+  //               // Если "пробел" нажат дважды
+  //               if (spaceCount === 1) {
+  //                 // Находим конец марки
+  //                 const resolvedPos = state.doc.resolve(to);
+  //                 const endOfMark = resolvedPos.end();
+
+  //                 // Перемещаем каретку за пределы марки
+  //                 view.dispatch(
+  //                   view.state.tr.setSelection(
+  //                     TextSelection.create(view.state.doc, endOfMark)
+  //                   )
+  //                 );
+
+  //                 spaceCount = 0; // Сбрасываем счётчик
+  //                 event.preventDefault(); // Отменяем стандартное поведение
+  //                 return true; // Предотвращаем дальнейшую обработку
+  //               }
+  //             } else {
+  //               spaceCount = 0; // Сбрасываем счётчик, если каретка вне марки
+  //             }
+  //           } else {
+  //             spaceCount = 0; // Сбрасываем счётчик при нажатии других клавиш
+  //           }
+
+  //           return false; // Продолжаем стандартную обработку
+  //         },
+  //       },
+  //     }),
+  //   ];
+  // },
 });
