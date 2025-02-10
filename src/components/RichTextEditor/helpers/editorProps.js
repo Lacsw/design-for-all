3503387e716f4ebf4aaeccec602f4e7c6d2c9bc1
Kargobober <kargobober@yandex.ru>
2025-02-11 00:57:46 +0300

    feat: rte/link - handle double space

--- src/components/RichTextEditor/helpers/editorProps.js
+++ src/components/RichTextEditor/helpers/editorProps.js
@@ -1,5 +1,8 @@
 // @ts-check
 import { goThroughLink } from '../extensions/link/helpers';
+// import { isActive, isMarkActive } from '@tiptap/core';
+
+let spaceCount = 0;
 
 /** @type {import('@tiptap/core').EditorOptions['editorProps']} */
 export const editorProps = {
@@ -28,5 +31,48 @@ export const editorProps = {
         });
       }
     }
+  },
+
+  handleKeyDown(view, event) {
+    const { state } = view;
+    const { selection } = state;
+    const { from, to } = selection;
+
+    if (event.key === ' ') {
+      const isLink = state.doc.rangeHasMark(
+        from - 1,
+        to,
+        state.schema.marks.link
+      );
+
+      // const nextIsLink = state.doc.rangeHasMark(
+      //   from,
+      //   to + 1,
+      //   state.schema.marks.link
+      // );
+      // console.log(isLink, nextIsLink);
+
+      if (isLink) {
+        spaceCount++;
+
+        if (spaceCount === 2) {
+          event.preventDefault();
+
+          const tr = view.state.tr;
+          const textNode = state.schema.text(' ');
+          tr.replaceWith(to - 1, to, textNode);
+          view.dispatch(tr);
+
+          spaceCount = 0;
+          return true;
+        }
+      } else {
+        spaceCount = 0;
+      }
+    } else {
+      spaceCount = 0;
+    }
+
+    return false;
   },
 };
