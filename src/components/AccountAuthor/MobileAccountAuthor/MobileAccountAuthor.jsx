9eb1086a4393aa8e499fc5226edc13b2ed3cdb45
RubyVine01 <leraprokofevaiera1@gmail.com>
2025-02-28 09:48:13 +0200

    update logout in MobileAccountAuthor

--- src/components/AccountAuthor/MobileAccountAuthor/MobileAccountAuthor.jsx
+++ src/components/AccountAuthor/MobileAccountAuthor/MobileAccountAuthor.jsx
@@ -7,9 +7,11 @@ import { getCurrentTheme, getCurrentUser } from 'store/selectors';
 import Button from 'components/Button/Button';
 import mobileAccountImgLight from 'images/account/mobile/mobile-account-img-light.svg';
 import mobileAccountImgDark from 'images/account/mobile/mobile-account-img-dark.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function MobileAccountAuthor({ hash, resetSection }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const theme = useSelector(getCurrentTheme);
 
   const handleLogout = useCallback(async () => {
@@ -17,10 +19,11 @@ export default function MobileAccountAuthor({ hash, resetSection }) {
       await authApi.logout();
       resetSection();
       dispatch(signInSuccess(null));
+      navigate('/');
     } catch (err) {
       console.error('Ошибка при выходе:', err);
     }
-  }, [dispatch, resetSection]);
+  }, [dispatch, resetSection, navigate]);
 
   if (!getCurrentUser) return null;
 
