69c7fc071875b5f063e14706cf620845735835ac
RubyVine01 <leraprokofevaiera1@gmail.com>
2025-03-17 20:41:35 +0200

    update search

--- src/utils/helpers/search.js
+++ src/utils/helpers/search.js
@@ -2,7 +2,11 @@ import { splitPaths } from './createTree';
 
 export default function searchArticles(text, articles) {
   const withSplit = splitPaths(articles);
-  const withWeight = withSplit.map((item) => ({ ...item, weight: 0, marked: [] }));
+  const withWeight = withSplit.map((item) => ({
+    ...item,
+    weight: 0,
+    marked: [],
+  }));
   setWeights(withWeight, text);
   const matchArticles = withWeight.filter((item) => item.weight > 0);
   matchArticles.sort((a, b) => b.weight - a.weight);
@@ -11,36 +15,42 @@ export default function searchArticles(text, articles) {
 
 export function setWeights(articles, text) {
   const fullString = text.toLowerCase();
-  const words = fullString.split(' ');
+  const words = fullString.split(' ').filter(Boolean);
 
   articles.forEach((art) => {
+    const markedSet = new Set();
     art.categories.forEach((cat) => {
       const category = cat.toLowerCase();
-      const splitCategory = category.split(' ');
+      let highlighted = category;
       if (category === fullString) {
         art.weight += 100;
-        art.marked.push(`<b>${category}</b>`);
-      } else if (
-        words.length > 1 &&
-        splitCategory.length > 1 &&
-        category.includes(fullString)
-      ) {
+        highlighted = `<b>${category}</b>`;
+      } else if (words.length > 1 && category.includes(fullString)) {
         art.weight += 20;
-        art.marked.push(category.replace(fullString, `<b>${fullString}</b>`));
+        highlighted = category.replace(
+          new RegExp(fullString, 'gi'),
+          `<b>${fullString}</b>`
+        );
       } else {
-        splitCategory.forEach((catWord) => {
-          words.forEach((inputWord) => {
-            if (catWord === inputWord) {
-              art.weight += 10;
-              art.marked.push(category.replace(inputWord, `<b>${inputWord}</b>`));
-            } else if (words.length === 1 && catWord.includes(inputWord)) {
-              art.weight += 1;
-              art.marked.push(category.replace(inputWord, `<b>${inputWord}</b>`));
-            } else art.marked.push(category);
-          });
+        words.forEach((inputWord) => {
+          if (category === inputWord) {
+            art.weight += 10;
+            highlighted = category.replace(
+              new RegExp(inputWord, 'gi'),
+              `<b>${inputWord}</b>`
+            );
+          } else if (words.length === 1 && category.includes(inputWord)) {
+            art.weight += 1;
+            highlighted = category.replace(
+              new RegExp(inputWord, 'gi'),
+              `<b>${inputWord}</b>`
+            );
+          }
         });
       }
+      markedSet.add(highlighted);
     });
+    art.marked = Array.from(markedSet);
   });
 }
 
