4bd4766239aa06a9852c4c68ea7a1ff8121cad6f
Kargobober <kargobober@yandex.ru>
2025-04-05 23:04:08 +0300

    fix[rte]: autolink for emails on input TODO: fix autolink on paste

--- src/components/RichTextEditor/extensions/link/config.js
+++ src/components/RichTextEditor/extensions/link/config.js
@@ -1,12 +1,13 @@
 // @ts-check
-import { disallowedDomains, disallowedProtocols } from './constants';
+import { disallowedDomains } from './constants';
+import { find as findLinks } from 'linkifyjs';
 
 /** @type {Partial<import('@tiptap/extension-link').LinkOptions>} */
 export const linkExtConfig = {
   autolink: true,
-  linkOnPaste: true,
+  linkOnPaste: false,
   openOnClick: true,
-  protocols: ['http', 'https'],
+  protocols: [], // custom protocols
   defaultProtocol: 'https',
   HTMLAttributes: {
     class: 'link_type_in-text rte__mark rte__mark_link',
@@ -20,56 +21,21 @@ export const linkExtConfig = {
     // contentEditable: true,
   },
 
-  isAllowedUri: (url, ctx) => {
-    try {
-      // construct URL
-      const parsedUrl = url.includes(':')
-        ? new URL(url)
-        : new URL(`${ctx.defaultProtocol}://${url}`);
-
-      // use default validation
-      if (!ctx.defaultValidate(parsedUrl.href)) {
-        return false;
-      }
-
-      // disallowed protocols
-      const protocol = parsedUrl.protocol.replace(':', '');
-
-      if (disallowedProtocols.includes(protocol)) {
-        return false;
-      }
-
-      // only allow protocols specified in ctx.protocols
-      const allowedProtocols = ctx.protocols.map((prot) =>
-        typeof prot === 'string' ? prot : prot.scheme
-      );
-
-      if (!allowedProtocols.includes(protocol)) {
-        return false;
-      }
-
-      // disallowed domains
-      const domain = parsedUrl.hostname;
-
-      if (disallowedDomains.includes(domain)) {
-        return false;
-      }
-
-      // all checks have passed
-      return true;
-    } catch {
-      return false;
-    }
-  },
-
   shouldAutoLink: (url) => {
+    const result = findLinks(url)[0];
+
+    switch (result.type) {
+      case 'email':
+        return false;
+      default:
+        break;
+    }
+
     try {
-      // construct URL
       const parsedUrl = url.includes(':')
         ? new URL(url)
         : new URL(`https://${url}`);
 
-      // only auto-link if the domain is not in the disallowed list
       const domain = parsedUrl.hostname;
 
       return !disallowedDomains.includes(domain);
