39985e49ef16fbdafd1f01a8af9774170a541341
Kargobober <kargobober@yandex.ru>
2025-04-11 01:22:35 +0300

    fs

--- src/store/slices/index.js
+++
@@ -1,6 +0,0 @@
-// когда слайс не имеет именновых экспортов, то еслинт начинает ругаться, хоть всё и работет
-/* eslint-disable import/export */
-import _userReducer from './user/slice';
-
-export const userReducer = _userReducer;
-export * from './user/slice';
