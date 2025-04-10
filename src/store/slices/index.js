1acaadd454a86d2983f543aaaca2540b74288e49
Kargobober <kargobober@yandex.ru>
2025-04-11 00:52:58 +0300

    fs

--- src/store/slices/index.js
+++ src/store/slices/index.js
@@ -1,12 +1,12 @@
 // когда слайс не имеет именновых экспортов, то еслинт начинает ругаться, хоть всё и работет
 /* eslint-disable import/export */
 import _userReducer from './userSlice';
-import _themeReducer from './theme';
+import _themeReducer from './theme/slice';
 
 export const userReducer = _userReducer;
 export * from './userSlice';
 
 export const themeReducer = _themeReducer;
-export * from './theme';
+export * from './theme/slice';
 
 export { default as articleReducer } from './articleSlice';
